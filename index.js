d7641121ff8d67beda3c550926748719587516d6
root <julien.taverne@gmail.com>
2021-01-20 00:22:30 +0000

    add inlineQuery

--- index.js
+++ index.js
@@ -19,13 +19,38 @@ app.get("/", function(req, res) {
 app.post("/www", function(req, res) {
 	const { message } = req.body
 
+
+let update = JSON.parse(request['postData']['contents']);
+          
+    // Make sure this is update is a type message
+    if (update.hasOwnProperty('inline_query')) {
+      let inlineQuery = update['inline_query'];
+  //    handleInlineQuery(inlineQuery);
+              
+    } else if(update.hasOwnProperty('message')) {
+      let msg = update['message'];
+      
+      if(msg.hasOwnProperty('text')) {
+        if(msg['text'].indexOf('/start') > -1) {
+	console.log(msg);
+          //handleStart(msg);
+        } else {
+          if(!msg.hasOwnProperty('via_bot')) {
+                console.log(msg);
+		//handleMessageDefault(msg);
+          }
+        }
+      }
+    }
+
+
 	//Each message contains "text" and a "chat" object, which has an "id" which is the chat id
 
-	if (!message || message.text.toLowerCase().indexOf("marco") < 0) {
+/*	if (!message || message.text.toLowerCase().indexOf("marco") < 0) {
 		// In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
 		return res.end()
 	}
-
+*/
 	// If we've gotten this far, it means that we have received a message containing the word "marco".
 	// Respond by hitting the telegram bot API and responding to the appropriate chat_id with the word "Polo!!"
 	// Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
@@ -34,7 +59,7 @@ app.post("/www", function(req, res) {
 			"https://api.telegram.org/bot1544386242:AAGn7SBWQGrUn45hk2nJMwV6bhxToMpCI_A/sendMessage",
 			{
 				chat_id: message.chat.id,
-				text: "Polo!! v3",
+				text: "Polo!! v4,update:"+update+"|inlineQuery:"+inlineQuery+"|msg:"+msg,
 			}
 		)
 		.then((response) => {
